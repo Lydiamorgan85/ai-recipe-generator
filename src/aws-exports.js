d23f27274c8e57fd295ef885e247a6f6cfa2c469
Lydia Morgan <Lydiamorgan000@gmail.com>
2026-01-29 12:26:21 +0000

    Add authentication

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsconfig = {
+    "aws_project_region": "eu-west-1",
+    "aws_cognito_region": "eu-west-1",
+    "aws_user_pools_id": "eu-west-1_vddu6feKn",
+    "aws_user_pools_web_client_id": "s4husf6h9avq9isg3eclptnk8",
+    "oauth": {}
+};
+
+export default awsconfig;
